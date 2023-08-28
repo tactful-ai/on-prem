2900bb3fde70c4832db85484cc450ae0dbbb6604
Waer1 <elwaeryousef@gmail.com>
2023-08-28 12:42:36 +0300

    update longhorn installation to use helm & installl important prequesets

--- scripts/install_configure_longhorn.sh
+++ scripts/install_configure_longhorn.sh
@@ -3,18 +3,31 @@
 source ./config.sh
 source ./secrets/vms_info.sh
 
-NUMBER_OF_NODES=${#node_info[@]}
+NUMBER_OF_NODES=$((${#node_info[@]} - 1))
 
-STORAGE_CLASS_FILE=$LONGHORN_FILES_LOCATION/storageclass.yaml
-LONGHORN_FILE=$LONGHORN_FILES_LOCATION/longhorn.yaml
+LONGHORN_VALUES_FILE=$LONGHORN_FILES_LOCATION/values.yaml
 
-yq eval --inplace '.parameters.numberOfReplicas = "'"${NUMBER_OF_NODES}"'"' -i "$STORAGE_CLASS_FILE"
+helm repo add longhorn https://charts.longhorn.io
 
+helm repo update
 
-yq eval --inplace '.metadata.name = "'"${LONGHORN_STORAGE_CLASS_NAME}"'"' -i "$STORAGE_CLASS_FILE"
+helm show values longhorn/longhorn > $LONGHORN_VALUES_FILE
 
-kubectl apply -f "$LONGHORN_FILE"
+yq eval --inplace '.persistence.defaultClassReplicaCount = "'"${NUMBER_OF_NODES}"'"' -i "$LONGHORN_VALUES_FILE"
 
-kubectl apply -f "$STORAGE_CLASS_FILE"
+helm install longhorn longhorn/longhorn --namespace longhorn-system -f $LONGHORN_VALUES_FILE --atomic
 
-kubectl patch storageclass longhorn  -p '{"metadata": {"annotations": {"storageclass.kubernetes.io/is-default-class": "true"}}}'
+while true; do
+    RUNNING_PODS=$(kubectl get pods -n longhorn-system -o jsonpath='{.items[*].status.phase}' | tr ' ' '\n' | grep 'Running' | wc -l)
+    TOTAL_PODS=$(kubectl get pods -n longhorn-system --no-headers | wc -l)
+
+    if [ "$RUNNING_PODS" -eq "$TOTAL_PODS" ]; then
+        echo "All pods are running."
+        break
+    else
+        echo "Waiting for pods to be in the 'Running' state..."
+        sleep 5
+    fi
+done
+
+curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.5.1/scripts/environment_check.sh | bash
