0982663a3c34493bc3f03d093e1aa0bdac5de34d
Waer1 <elwaeryousef@gmail.com>
2023-09-07 12:42:34 +0300

    split longhorn pre to run independentally from the rke pre

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -15,22 +15,35 @@ fi
 # Create a new YAML file with initial content
 echo "" > "$INVENTORY_FILE"
 
-# Start building the YAML content using yq
+# define the local host as the first vm in the inventory
 yq eval '.local_device.hosts.localhost.ansible_connection = "local"' -i "$INVENTORY_FILE"
 yq eval ".local_device.hosts.localhost.ansible_become_pass = \"$sudo_password\"" -i "$INVENTORY_FILE"
 
 
-for ((i=0; i<num_nodes; i++)); do
+# define the master node in the inventory
+node="${node_info[0]}"
+IFS='|' read -ra info <<< "$node"
+hostname=master$((i+1))
+ansible_host="${info[0]}"
+ansible_ssh_user="${info[2]}"
+root_password="${info[1]}"
+echo "Adding $hostname to the inventory file"
+
+yq eval ".master_nodes.hosts.$hostname.ansible_host = \"$ansible_host\"" -i "$INVENTORY_FILE"
+yq eval ".master_nodes.hosts.$hostname.ansible_ssh_private_key_file = \"$SSH_KEY_PATH\"" -i "$INVENTORY_FILE"
+yq eval ".master_nodes.hosts.$hostname.ansible_ssh_user = \"$ansible_ssh_user\"" -i "$INVENTORY_FILE"
+
+
+for ((i=1; i<num_nodes; i++)); do
     node="${node_info[$i]}"
     IFS='|' read -ra info <<< "$node"
-    hostname=${info[3]}
+    hostname=node$((i+1))
     ansible_host="${info[0]}"
     ansible_ssh_user="${info[2]}"
     root_password="${info[1]}"
     echo "Adding $hostname to the inventory file"
 
-    yq eval ".external_vms.hosts.$hostname.ansible_host = \"$ansible_host\"" -i "$INVENTORY_FILE"
-    yq eval ".external_vms.hosts.$hostname.ansible_ssh_private_key_file = \"$SSH_KEY_PATH\"" -i "$INVENTORY_FILE"
-    yq eval ".external_vms.hosts.$hostname.ansible_ssh_user = \"$ansible_ssh_user\"" -i "$INVENTORY_FILE"
-    yq eval ".external_vms.hosts.$hostname.ansible_become_pass = \"$root_password\"" -i "$INVENTORY_FILE"
+    yq eval ".worker_nodes.hosts.$hostname.ansible_host = \"$ansible_host\"" -i "$INVENTORY_FILE"
+    yq eval ".worker_nodes.hosts.$hostname.ansible_ssh_private_key_file = \"$SSH_KEY_PATH\"" -i "$INVENTORY_FILE"
+    yq eval ".worker_nodes.hosts.$hostname.ansible_ssh_user = \"$ansible_ssh_user\"" -i "$INVENTORY_FILE"
 done
