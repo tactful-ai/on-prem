c4a43a861a51134d32271bde545382a1a80544f0
Waer1 <elwaeryousef@gmail.com>
2023-09-09 11:42:04 +0300

    comditionallyy install rke in the jump server

--- scripts/jump_server_prerequisites.sh
+++ scripts/jump_server_prerequisites.sh
@@ -2,6 +2,7 @@
 
 # Load the information from the separate files
 source ./user_fill.sh
+source ./config.sh
 
 # install yq if not installed
 if ! command -v yq &>/dev/null; then
@@ -28,18 +29,27 @@ fi
 # generate inventory file for ansible
 source ./scripts/generate_inventory.sh
 
-RKE_VERSION=$(curl -s https://api.github.com/repos/rancher/rke/releases | \
-  grep -E '"tag_name": "v[0-9]+\.[0-9]+\.[0-9]+"' | \
-  grep -v -E '"tag_name": "v[0-9]+\.[0-9]+\.[0-9]-rc[0-9]+"' | \
-  head -n 1 | \
-  cut -d '"' -f 4)
 
-if [ -z "$RKE_VERSION" ]; then
-  echo "Error: Unable to fetch the latest RKE LTS version."
-  exit 1
+if [ "$RKE_VERSION" = "rke1" ]; then
+  echo "enable rke1"
+  # RKE_VERSION=$(curl -s https://api.github.com/repos/rancher/rke/releases | \
+  # grep -E '"tag_name": "v[0-9]+\.[0-9]+\.[0-9]+"' | \
+  # grep -v -E '"tag_name": "v[0-9]+\.[0-9]+\.[0-9]-rc[0-9]+"' | \
+  # head -n 1 | \
+  # cut -d '"' -f 4)
+RKE_VERSION="v1.0.4"
+  if [ -z "$RKE_VERSION" ]; then
+    echo "Error: Unable to fetch the latest RKE LTS version."
+    exit 1
+  fi
+  yq e ".[].vars.rke_version = \"${RKE_VERSION}\" " -i $JUMP_SERVER_PLAYBOOK_LOCATION
+  yq e ".[].vars.install_rke = true " -i $JUMP_SERVER_PLAYBOOK_LOCATION
+elif [ "$RKE_VERSION" = "rke2" ]; then
+  echo "disable rke1"
+  yq e ".[].vars.install_rke = false " -i $JUMP_SERVER_PLAYBOOK_LOCATION
 fi
 
-yq e ".[].vars.rke_version = \"${RKE_VERSION}\" " -i $JUMP_SERVER_PLAYBOOK_LOCATION
+
 
 # install prerequisites for jump server
 ansible-playbook -i ./playbooks/inventory.yml ./playbooks/jump_server_prerequisites.yml
