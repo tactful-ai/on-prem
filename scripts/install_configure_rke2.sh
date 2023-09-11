45effb45493630702dfe871fb9bfa6b7539c8660
Yousef Al-waer <elwaeryousef@gmail.com>
2023-09-11 08:46:08 +0300

    update master master node taint

--- scripts/install_configure_rke2.sh
+++ scripts/install_configure_rke2.sh
@@ -29,85 +29,81 @@ yq e '.["tls-san"] += [env(ip_address)]' -i $MASTER_CONFIG
 yq e ".[\"cni\"] += [\"$NETWORK_PLUGIN\"]" -i $MASTER_CONFIG
 
 yq e ".node.hostnameOverride = \"master-node\"" -i $MASTER_CONFIG
-yq e ".node-taint.taints[0].key = \"controlplane\"" -i $MASTER_CONFIG
-yq e ".node-taint.taints[0].value = \"NoSchedule\"" -i $MASTER_CONFIG
-yq e ".node-taint.taints[0].effect = \"NoSchedule\"" -i $MASTER_CONFIG
+yq e ".node.hostnameOverride[0] = \"CriticalAddonsOnly=true:NoExecute\"" -i $MASTER_CONFIG
 
-yq e ".node-taint.taints[1].key = \"etcd\"" -i $MASTER_CONFIG
-yq e ".node-taint.taints[1].value = \"NoExecute\"" -i $MASTER_CONFIG
-yq e ".node-taint.taints[1].effect = \"NoExecute\"" -i $MASTER_CONFIG
+
 
 yq e ".[].tasks[1].copy.src = \"${MASTER_CONFIG}\" " -i $MASTER_PLAYBOOK
-yq e ".[].tasks[6].fetch.dest = \"${CLUSTER_TOKEN_LOCATION}\" " -i $MASTER_PLAYBOOK
-yq e ".[].tasks[7].fetch.dest = \"${CLUSTER_CONFIG_LOCATION}\" " -i $MASTER_PLAYBOOK
+yq e ".[].tasks[7].fetch.dest = \"${CLUSTER_TOKEN_LOCATION}\" " -i $MASTER_PLAYBOOK
+yq e ".[].tasks[8].fetch.dest = \"${CLUSTER_CONFIG_LOCATION}\" " -i $MASTER_PLAYBOOK
 
-ansible-playbook -i $ANSIBLE_INVENTORY_FILE $MASTER_PLAYBOOK
-
-
-yq e ".clusters[0].cluster.server = \"https://${ip_address}:6443\" " -i $CLUSTER_CONFIG_LOCATION
+# ansible-playbook -i $ANSIBLE_INVENTORY_FILE $MASTER_PLAYBOOK
 
 
-YAML_FILE=$CLUSTER_FILES_LOCATION/worker.yml
-WORKERS_PLAYBOOK=${ANSIBLE_PLAYBOOKS_LOCATION}/install_rke2_worker.yml
-WORKER_CONFIG=$CLUSTER_FILES_LOCATION/worker.yml
+# yq e ".clusters[0].cluster.server = \"https://${ip_address}:6443\" " -i $CLUSTER_CONFIG_LOCATION
 
 
-# Remove the existing YAML file if it exists
-if [ -f "$YAML_FILE" ]; then
-    rm "$YAML_FILE"
-fi
-
-# Create a YAML file with server and token fields
-cat <<EOL > "$YAML_FILE"
-server: https://$ip_address:9345
-token: $(cat $CLUSTER_TOKEN_LOCATION)
-EOL
-
-# Use yq to validate and format the YAML file (optional)
-yq eval '.' -i "$YAML_FILE"
-
-yq e ".[].tasks[1].copy.src = \"${WORKER_CONFIG}\" " -i $WORKERS_PLAYBOOK
-
-ansible-playbook -i $ANSIBLE_INVENTORY_FILE $WORKERS_PLAYBOOK
-
-# export the KUBECONFIG environment variable
-export KUBECONFIG=$CLUSTER_FILES_LOCATION/kube_config_cluster.yml
+# YAML_FILE=$CLUSTER_FILES_LOCATION/worker.yml
+# WORKERS_PLAYBOOK=${ANSIBLE_PLAYBOOKS_LOCATION}/install_rke2_worker.yml
+# WORKER_CONFIG=$CLUSTER_FILES_LOCATION/worker.yml
 
 
-# Number of nodes expected to be in "Ready" state
-expected_nodes_count=$num_nodes
+# # Remove the existing YAML file if it exists
+# if [ -f "$YAML_FILE" ]; then
+#     rm "$YAML_FILE"
+# fi
 
-# Number of retries
-retries=500
+# # Create a YAML file with server and token fields
+# cat <<EOL > "$YAML_FILE"
+# server: https://$ip_address:9345
+# token: $(cat $CLUSTER_TOKEN_LOCATION)
+# EOL
 
-# Delay between retries (in seconds)
-delay=5
+# # Use yq to validate and format the YAML file (optional)
+# yq eval '.' -i "$YAML_FILE"
 
-all_nodes_ready=false  # Initialize the variable as false
+# yq e ".[].tasks[1].copy.src = \"${WORKER_CONFIG}\" " -i $WORKERS_PLAYBOOK
 
-for ((i = 0; i < retries; i++)); do
-    # Execute the kubectl command to get node status
-    nodes_status=$(kubectl get nodes --no-headers)
+# ansible-playbook -i $ANSIBLE_INVENTORY_FILE $WORKERS_PLAYBOOK
 
-    # Count the number of nodes in the "Ready" state
-    ready_count=$(echo "$nodes_status" | awk '$2 == "Ready" { count++ } END { print count }')
+# # export the KUBECONFIG environment variable
+# export KUBECONFIG=$CLUSTER_FILES_LOCATION/kube_config_cluster.yml
 
-    echo "Number of nodes in 'Ready' state: $ready_count"
 
-    # Check if all nodes are in the "Ready" state
-    total_nodes=$(echo "$nodes_status" | wc -l)
-    if [ "$ready_count" -eq "$total_nodes" ]; then
-        echo "All nodes are ready."
-        all_nodes_ready=true  # Set the variable to true
-        break
-    fi
+# # Number of nodes expected to be in "Ready" state
+# expected_nodes_count=$num_nodes
 
-    # Sleep for the specified delay before the next retry
-    sleep $delay
-done
+# # Number of retries
+# retries=500
 
-# Check if all nodes are ready; if not, throw an error
-if [ "$all_nodes_ready" = false ]; then
-    echo "Error: Not all nodes are ready."
-    exit 1
-fi
+# # Delay between retries (in seconds)
+# delay=5
+
+# all_nodes_ready=false  # Initialize the variable as false
+
+# for ((i = 0; i < retries; i++)); do
+#     # Execute the kubectl command to get node status
+#     nodes_status=$(kubectl get nodes --no-headers)
+
+#     # Count the number of nodes in the "Ready" state
+#     ready_count=$(echo "$nodes_status" | awk '$2 == "Ready" { count++ } END { print count }')
+
+#     echo "Number of nodes in 'Ready' state: $ready_count"
+
+#     # Check if all nodes are in the "Ready" state
+#     total_nodes=$(echo "$nodes_status" | wc -l)
+#     if [ "$ready_count" -eq "$total_nodes" ]; then
+#         echo "All nodes are ready."
+#         all_nodes_ready=true  # Set the variable to true
+#         break
+#     fi
+
+#     # Sleep for the specified delay before the next retry
+#     sleep $delay
+# done
+
+# # Check if all nodes are ready; if not, throw an error
+# if [ "$all_nodes_ready" = false ]; then
+#     echo "Error: Not all nodes are ready."
+#     exit 1
+# fi
