fc45f303ae9959dd202a52329f9fb20d27eb597b
Yousef Al-waer <elwaeryousef@gmail.com>
2023-09-17 18:35:28 +0300

    add static link for the k8s  dashboard

--- scripts/install_configure_k8s_dashboard.sh
+++ scripts/install_configure_k8s_dashboard.sh
@@ -15,7 +15,7 @@ K8S_TOKEN=$(kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={"
 
 service_link=$(get_service_info kubernetes-dashboard)
 
-add_service_to_readme "K8s Dashboard Link" $service_link $K8S_TOKEN
+add_service_to_readme "K8s Dashboard Link" "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/." $K8S_TOKEN
 
 print_label "Kubernetes Dashboard installed, and admin token stored in ./token/admin-token.txt" 2
 
