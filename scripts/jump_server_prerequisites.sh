6b47290a43d0c18829b3650cc1515717d2fd4e88
Yousef Al-waer <elwaeryousef@gmail.com>
2023-09-23 21:57:10 +0300

    update syntax error in the rke2 version naming

--- scripts/jump_server_prerequisites.sh
+++ scripts/jump_server_prerequisites.sh
@@ -127,7 +127,7 @@ grep -E '"tag_name"' | \
 grep -v -E '"tag_name": "v[0-9]+\.[0-9]+\.[0-9]-rc[0-9]+"' | \
 head -n 1 | \
 cut -d '"' -f 4)
-yq e "RKE2_version = \"${RKE2_VERSION}\" " -i $ANSIBLE_ENVIRONMENT_FILE
+yq eval ".RKE2_version = \"${RKE2_VERSION}\"" -i $ANSIBLE_ENVIRONMENT_FILE
 fi
 
 # installing the ansible.posix community.general to ensure that the firewall and ufw modules are available
